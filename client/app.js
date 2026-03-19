b81a58aa61bd04c50796c03a16075b80d3e79c76
Simran <simran.dalv30@gmail.com>
2026-03-20 00:46:37 +0530

    Hosted locally using NGINX

--- client/app.js
+++ client/app.js
@@ -1,52 +1,39 @@
-const server = "http://127.0.0.1:5000";
+// const server = "http://127.0.0.1:5000"
 
-window.onload = function () {
-    fetch(server + "/get_location_names")
+window.onload = () =>{
+    fetch("/api/get_location_names")
         .then(response => response.json())
-        .then(data => {
-
+        .then(data =>{
             const locations = data.locations;
             const locationSelect = document.getElementById("location");
 
             locations.forEach(location => {
-
                 let option = document.createElement("option");
                 option.value = location;
                 option.text = location;
 
                 locationSelect.appendChild(option);
             });
-
         });
 };
 
-
 function predictPrice() {
-
     const sqft = document.getElementById("sqft").value;
     const bhk = document.getElementById("bhk").value;
     const bath = document.getElementById("bath").value;
     const location = document.getElementById("location").value;
 
-    const formData = new FormData();
-
+    const formData =new FormData();
     formData.append("total_sqft", sqft);
     formData.append("bhk", bhk);
     formData.append("bath", bath);
     formData.append("location", location);
 
-    fetch(server + "/predict_home_price", {
-        method: "POST",
-        body: formData
-    })
+    fetch("/api/predict_home_price", {method: "POST", body : formData})
         .then(response => response.json())
-        .then(data => {
-
+        .then(data=> {
             const price = data.estimated_price;
+            document.getElementById("result").innerHTML = "Estimated Price: ₹ " + price + " Lakh";
 
-            document.getElementById("result").innerHTML =
-                "Estimated Price: ₹ " + price + " Lakh";
-
-        });
-
+        })
 }
